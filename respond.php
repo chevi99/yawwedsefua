66507ce97e650fd25b19d0673412411a38e82a0f
towa <denns4life@gmail.com>
2017-04-14 13:31:11 +0100

    Added gif loader to this page

--- respond.php
+++ respond.php
@@ -10,6 +10,7 @@ $response = mysqli_real_escape_string($con, $_POST['respond']);
 //insert into db
 $insertQuery = mysqli_query($con, "INSERT INTO response_t(browser, response) VALUES('MOBILE', '$response')") or die(mysqli_error($con));
 if ($insertQuery) {
+    echo '<div><img src="assets/images/ajaxLoader.gif" class="img-thumbnail"></div>';
     echo '<h5>THANK YOU FOR YOUR RESPONSE</h5>';
     echo '<script>
           var responseStatus = "Yes";
